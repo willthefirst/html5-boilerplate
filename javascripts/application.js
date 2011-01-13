6953830402a3251312dd1ab24759c047c01165a7
Jason Garber <jason@sixtwothree.org>
2011-01-13 15:47:11 -0500

    Moved js and css to javascripts and stylesheets. Added Eric Meyer's latest CSS reset, added jQuery.

---
+++ javascripts/application.js
@@ -0,0 +1,30 @@
+var NAMESPACE = NAMESPACE || {
+	common: {
+		init: function() {
+			
+		}
+	}
+};
+
+var UTIL = UTIL || {
+	exec: function( controller, action ) {
+		var ns = NAMESPACE,
+			act = ( action === undefined ) ? "init" : action;
+		
+		if ( controller !== "" && ns[controller] && typeof( ns[controller][act] ) == "function" ) {
+			ns[controller][act]();
+		}
+	},
+	
+	init: function() {
+		var body = document.body,
+			controller = body.getAttribute( "data-controller" ),
+			action = body.getAttribute( "data-action" );
+		
+		UTIL.exec( "common" );
+		UTIL.exec( controller );
+		UTIL.exec( controller, action );
+	}
+};
+
+$().ready( UTIL.init );
